8278555436b0a9a6607a6bc437cb2e99d07caf8a
Anass <elliveed@gmail.com>
2022-03-13 23:34:36 +0100

    list case 15

--- src/test/java/FizzBuzzListGeneratorTest.java
+++ src/test/java/FizzBuzzListGeneratorTest.java
@@ -46,13 +46,25 @@ public class FizzBuzzListGeneratorTest {
         Assert.assertEquals(expected, result);
     }
 
-    
+
     @Test
     public void FizzBuzzListReturns1_2_Fizz_4_BuzzIfInputIs5(){
         int a=5;
 
         List<String> expected = new ArrayList<String >() {{
             add("1");add("2");add("Fizz");add("4");add("Buzz");
+        }};
+
+        List<String> result = FizzBuzzListGenerator.generateList(a);
+
+        Assert.assertEquals(expected, result);
+    }
+    @Test
+    public void FizzBuzzListReturns1_2_to_14_FizzBuzzIfInputIs15(){
+        int a=15;
+
+        List<String> expected = new ArrayList<String >() {{
+            add("1");add("2");add("Fizz");add("4");add("Buzz");add("Fizz");add("7");add("8");add("Fizz");add("Buzz");add("11");add("Fizz");add("13");add("14");add("FizzBuzz");
         }};
 
         List<String> result = FizzBuzzListGenerator.generateList(a);
